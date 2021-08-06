8dcc47d341433fda4d3a42cf7e0cd377e512bf17
syoch <hacksyoch@gmail.com>
2021-08-07 08:33:36 +0900

    update: edit

--- symbol_analyzer.py
+++ symbol_analyzer.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 import string
 
 
@@ -11,6 +11,10 @@ def tokenize(src: str) -> List[str]:
     while src:
         ch = src[0]
         next = src[1] if len(src) > 1 else None
+
+        if ch in string.whitespace:
+            src = src[1:]
+            continue
 
         token = ""
         if is_identifier(ch):
@@ -29,5 +33,36 @@ def tokenize(src: str) -> List[str]:
     return tokens
 
 
+class FuncName():
+    def __init__(self) -> None:
+        self.is_static = False
+        self.namespaces = []
+        self.name = ""
+        self.args = []
+        self.return_type = ""
+
+
+class Type():
+    def __init__(self) -> None:
+        self.length = 0
+        self.name = ""
+        self.args = []
+        self.return_type = ""
+
+
+def read_type(tokens: List[str]) -> Tuple[str, List[str]]:
+    pass
+
+
+def read_func(tokens: List[str]) -> Tuple[str, List[str]]:
+    is_static = False
+    if tokens[0:1] == ["static"]:
+        tokens = tokens[1:]
+        is_static = True
+    pass
+
+
 if __name__ == "__main__":
-    tokenize("hello1<in_t> std::exception::~exception(hello<int, void<t>>)")
+    tokens = tokenize(
+        "hello1<in_t> std::exception::operator +(hello<int, void<t>>)")
+    print(tokens)
