1ebad9335b107f71d084bd09c66b518f84976565
syoch <hacksyoch@gmail.com>
2021-09-06 20:26:35 +0900

    update: add functable maker

--- make.py
+++ make.py
@@ -1,10 +1,53 @@
-fp = open("demangled_functions")
-fp.readline()
-fp.readline()
-fp.readline()
-print(fp.readline()[9:-1])
-# for line in fp:
-#     addr = int(line[0:8], 16)
-#     name = line[9:].strip()
-#     print(addr, name)
-fp.close()
+import rpx
+import ghs_demangle as ghs_demangle
+import logging
+import json
+import pickle
+
+logging.basicConfig(level=logging.INFO)
+
+
+def get_name(obj: ghs_demangle.Function) -> str:
+    if type(obj) == str:
+        return obj
+
+    if type(obj.name) == str:
+        return obj.name
+
+    return obj.name.name
+
+
+demangler = ghs_demangle.Demangler()
+elf = rpx.Elf("/home/syoch/Documents/reversing/Minecraft.Client.rpx")
+
+tree = {}
+
+for func in elf.getFunctions():
+    mangled_name = func.name.decode()
+
+    if mangled_name == "__CPR74____ct__Q4_2nn3nex15StepSequenceJJob4StepFMQ3_2nn3nexJ16JFv_vPCw":
+        continue
+
+    f = demangler.demangle(mangled_name)
+    if isinstance(f, str):
+        continue
+
+    if isinstance(f.name, str):
+        continue
+
+    if not f.name.namespace.path:
+        continue
+    cursor = tree
+    for tmp in f.name.namespace.path:
+        part = tmp.name
+        if part not in cursor:
+            cursor[part] = {}
+        cursor = cursor[part]
+
+    if "funcs" not in cursor:
+        cursor["funcs"] = []
+
+    cursor["funcs"].append(f)
+
+with open("functable.pkl", "wb") as f:
+    pickle.dump(tree, f)
